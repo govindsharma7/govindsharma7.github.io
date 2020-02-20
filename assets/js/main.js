9444fc21e5017e6f2ed16c03496decaecba5376c
Govind Sharma <govind@dailyrounds.org>
2020-02-20 19:03:56 +0530

    fix: circle counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@
 $('body').scrollspy({ target: '.navmenu' });
 
 // When we click on the LI
-$("ul.qcontrols li").click(function(){
-  // If this isn't already active
-  if (!$(this).hasClass("active")) {
-    // Remove the class from anything that is active
-    $("ul.qcontrols li.active").removeClass("active");
-    // And make this active
-    $(this).addClass("active");
-  }
+$("ul.qcontrols li").click(function () {
+    // If this isn't already active
+    if (!$(this).hasClass("active")) {
+        // Remove the class from anything that is active
+        $("ul.qcontrols li.active").removeClass("active");
+        // And make this active
+        $(this).addClass("active");
+    }
 });
 
 
@@ -20,58 +20,58 @@ var wow = new WOW({
 });
 wow.init();
 
-jQuery(document).ready(function( $ ) {
-        $('.circle').circleProgress({
-        value: 0.70,
+jQuery(document).ready(function ($) {
+    $('.circle').circleProgress({
+        value: 0.90,
         startAngle: 4.75,
         size: 200,
         fill: { gradient: ["#FFFFFF"] }
-      });
-        $('.two').circleProgress({
-          value: 0.75,
-        });
-        $('.three').circleProgress({
-          value: 0.50,
-        });
-        $('.four').circleProgress({
-          value: 0.90,
-        });
+    });
+    $('.two').circleProgress({
+        value: 0.80,
+    });
+    $('.three').circleProgress({
+        value: 0.60,
+    });
+    $('.four').circleProgress({
+        value: 0.90,
+    });
 
-   //Clients carousel Slider
+    //Clients carousel Slider
     $('#clients-carousel').owlCarousel({
         navigation: false, // Show next and prev buttons
         slideSpeed: 400,
         paginationSpeed: 800,
         autoPlay: true,
-        pagination : false,
-        items : 4,
-        itemsCustom : false,
-        itemsDesktop : [1199,4],
-        itemsDesktopSmall : [980,3],
-        itemsTablet: [768,2],
+        pagination: false,
+        items: 4,
+        itemsCustom: false,
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 2],
         itemsTabletSmall: false,
-        itemsMobile : [479,1],
+        itemsMobile: [479, 1],
     });
-       $('#menu').click(function(){
-       $('#menu').not(this).removeClass("active");
-       $(this).toggleClass("active");
-      });
+    $('#menu').click(function () {
+        $('#menu').not(this).removeClass("active");
+        $(this).toggleClass("active");
+    });
 
-  });
+});
 
-  
+
 //ScrollTop
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
